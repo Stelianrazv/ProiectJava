595eab3e797dbf91f3f9c762745c44224e7ddfd1
Stelianrazv <32238891+Stelianrazv@users.noreply.github.com>
2017-10-23 19:46:26 +0300

    Update ListCar.java

--- ListCar.java
+++ ListCar.java
@@ -1,12 +1,9 @@
 package com.Inmatriculare.Auto;
 
 /* Fereastra List */
-import com.sun.prism.paint.*;
-
 import javax.swing.*;
 import javax.swing.table.JTableHeader;
 import java.awt.*;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
@@ -15,17 +12,20 @@ import java.sql.Statement;
 import java.util.Vector;
 
 public class ListCar extends JFrame {
-    JButton exitButton, backButton;
+    JButton exitButton, menuButton;
     Statement myStmt;
     Vector data, row, columnNames;
 
     public ListCar(Statement myStmt) {
         this.myStmt = myStmt;
+
         // Seteaza fereastra Register: titlu, mod de inchidere, dimensiuni si pozitia
-        this.setTitle("Inregistrare Auto");
+        this.setTitle("Evidenta Masinilor inregistrate");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(900,500);
+        this.setResizable(false);
         this.setLocationRelativeTo(null);
+
         // Creaza main panel panel principal care contine toate celelalte elemente
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
@@ -34,11 +34,9 @@ public class ListCar extends JFrame {
         JPanel listPanel = new JPanel();
         columnNames = new Vector();
         columnNames.add("ID");
-        columnNames.add("Prenume");
-        columnNames.add("Nume");
-        columnNames.add("Marca");
-        columnNames.add("Model");
-        columnNames.add("An");
+        columnNames.add("First Name");
+        columnNames.add("Last Name");
+        columnNames.add("Year");
 
         try {
             ResultSet myRs = myStmt.executeQuery("select * from masina");
@@ -54,10 +52,9 @@ public class ListCar extends JFrame {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         // Creaza tabel
         JTable jTable = new JTable(data, columnNames);
-        jTable.setRowHeight(jTable.getRowHeight() + 20);
+        jTable.setRowHeight(jTable.getRowHeight() + 10);
         jTable.setFont(new Font("Arial", Font.PLAIN, 18));
         JTableHeader header = jTable.getTableHeader();
         header.setFont(new Font("Arial", Font.BOLD, 18));
@@ -66,32 +63,32 @@ public class ListCar extends JFrame {
         listPanel.add(scrollPane);
         mainPanel.add(listPanel,BorderLayout.CENTER);
 
-
         // Creaza buttonPanel pentru butoane
         JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.BLACK);
         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10,20));
 
+        // Butonul "BACK"
+        menuButton = new JButton("MENIU");
+        menuButton.setPreferredSize(new Dimension(150,40));
+        menuButton.setFont(new Font("Arial", Font.PLAIN, 18));
+        menuButton.setBackground(Color.LIGHT_GRAY);
+
         // Butonul "EXIT"
-        exitButton = new JButton("Iesire");
+        exitButton = new JButton("EXIT");
         exitButton.setPreferredSize(new Dimension(150,40));
         exitButton.setFont(new Font("Arial", Font.PLAIN, 18));
         exitButton.setBackground(Color.LIGHT_GRAY);
 
-        // Butonul "BACK"
-        backButton = new JButton("Inapoi");
-        backButton.setPreferredSize(new Dimension(150,40));
-        backButton.setFont(new Font("Arial", Font.PLAIN, 18));
-        backButton.setBackground(Color.LIGHT_GRAY);
-
         // Face ca butoanele sa poata fi receptive la events
         ListenForButton listenForButton = new ListenForButton();
         exitButton.addActionListener(listenForButton);
-        backButton.addActionListener(listenForButton);
+        menuButton.addActionListener(listenForButton);
 
         //Adauga butoane la buttonPanel
+        buttonPanel.add(menuButton);
         buttonPanel.add(exitButton);
-        buttonPanel.add(backButton);
+
         // Adauga buttonPanel la mainPanel
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
         // Adauga mainPanel la frame
@@ -107,7 +104,7 @@ public class ListCar extends JFrame {
             if (e.getSource() == exitButton){
                 // Inchide fereastra List
                 System.exit(0);
-            } else if (e.getSource() == backButton) {
+            } else if (e.getSource() == menuButton) {
                 new WelcomeGui(myStmt);
                 dispose();
             }
