d9bbdf82582e2ab4c406a41bb50ce81c9269bbd1
Stelianrazv <32238891+Stelianrazv@users.noreply.github.com>
2017-10-27 21:07:05 +0300

    Update WelcomeGui.java

--- WelcomeGui.java
+++ WelcomeGui.java
@@ -1,6 +1,6 @@
 package com.Inmatriculare.Auto;
 
-/* Prima fereastra - Welcome */
+//* Prima fereastra - Welcome
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -18,14 +18,19 @@ public class WelcomeGui extends JFrame {
 
     public WelcomeGui(Statement myStmt) {
         this.myStmt = myStmt;
+
         // Titlul ferestrei
         this.setTitle("Inregistrare Auto");
+
         // Modalitatea de inchidere a ferestrei
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         // Marimea ferestrei
         this.setSize(1200,700);
+
         // Pozitioneaza fereastra in centrul ecranului
         this.setLocationRelativeTo(null);
+
         // Creaza mainPanel - panel principal care include toate elementele
         JLabel mainLabel = new JLabel();
         mainLabel.setSize(1200,700);
@@ -35,10 +40,12 @@ public class WelcomeGui extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         // Modifica dimensiunile la imaginea de background
         Image dimg = img.getScaledInstance(mainLabel.getWidth(), mainLabel.getHeight(),
                 Image.SCALE_SMOOTH);
         ImageIcon imageIcon = new ImageIcon(dimg);
+
         // Adauga imaginea de fond la mainLabel
         mainLabel.setIcon(imageIcon);
         mainLabel.setLayout(new BorderLayout());
@@ -50,19 +57,19 @@ public class WelcomeGui extends JFrame {
         // Precizeaza layout pentru buttonPanel
         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10,20));
 
-        //Butonul masini
+        // Butonul Masini
         masinaButton = new JButton("MASINI");
         masinaButton.setPreferredSize(new Dimension(150,40));
         masinaButton.setFont(new Font("Arial", Font.PLAIN, 18));
         masinaButton.setBackground(Color.LIGHT_GRAY);
 
-        //Butonul camion
+        // Butonul Camioane
         camionButton = new JButton("CAMIOANE");
         camionButton.setPreferredSize(new Dimension(150,40));
         camionButton.setFont(new Font("Arial", Font.PLAIN, 18));
         camionButton.setBackground(Color.LIGHT_GRAY);
 
-        // Butonul exit
+        // Butonul Exit
         exitButton = new JButton("EXIT");
         exitButton.setPreferredSize(new Dimension(150,40));
         exitButton.setFont(new Font("Arial", Font.PLAIN, 18));
@@ -86,29 +93,30 @@ public class WelcomeGui extends JFrame {
         this.pack();
         this.setVisible(true);
     }
-    // Pentru events
+
+    // Pentru eveniment
     private class ListenForButton implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
+
             // Sursa eventului este butonul "EXIT"
             if (e.getSource() == exitButton){
-                // Inchide fereastra Welcome
+                // Inchide fereastra WelcomeGui
                 System.exit(0);
 
             }// Sursa eventului este butonul "Masini"
             else if (e.getSource() == masinaButton) {
-            // Deschide fereastra Register
-            new RegisterCar( myStmt );
-            // Inchide fereastra Welcome
-            dispose();
+                // Deschide fereastra "Masini"
+                new RegisterCar( myStmt );
+                // Inchide fereastra "Masini"
+                dispose();
 
-        }// Sursa eventului este butonul "Camioane"
+            }// Sursa eventului este butonul "Camioane"
             else if (e.getSource() == camionButton) {
-            // Deschide fereastra Register
-            new RegisterCamion(myStmt);
-            // Inchide fereastra Welcome
-            dispose();
-        }
+                // Deschide fereastra "Camioane"
+                new RegisterCamion(myStmt);
+                // Inchide fereastra "Camioane"
+                dispose();
+            }
         }
     }
-}
